912fdfd30271b8f831ead4694d1dd69300013c93
Michal <michalruth144@gmail.com>
2022-12-04 23:18:25 +0200

    fix problems in output

--- main.c
+++ main.c
@@ -1,37 +1,40 @@
 #include <stdio.h>
 #include "my_mat.h"
-#include <math.h>
 
-double fmin(double x, double y);
-
-
-int main(){
+int main ()
+{
+    
     int mat[ROWS][COLUMNS];
-    char user_input;
-    // int i;
-    // int j;
-   // int i,j=0;
-    while(scanf("%c",&user_input)==1){ // until the user quits
-         
-          switch ((int) user_input)
+    char data;
+    int i,j;
+    while (scanf("%c", &data) == 1){
+        switch ((int) data)
         {
             case (int) 'A':
-                set_mat(mat); 
+                set_mat(mat);
                 get_shortest_path_matrix(mat);
                 break;
+
+           
+               // 
+                
             case (int) 'B':
-                //scanf("%d%d", &i, &j);
-                is_there_path(mat);
+                
+                scanf("%d", &i);
+                scanf("%d", &j);
+                is_there_path(mat,i,j);
                 break;
             case (int) 'C':
-                //scanf("%d%d", &i, &j);
-                shortest_path(mat);
+                
+                scanf("%d", &i);
+                scanf("%d", &j);
+                shortest_path(mat, i, j);
                 break;
-            case (int) 'D':
+        
+
+             case (int) 'D':
                 break;
-
-      }
-
+        }
     }
-     return 0;
+  return 0;
 }
