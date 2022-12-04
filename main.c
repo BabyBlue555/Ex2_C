0a52e7addf4378c9a476960dfa05bf235097168f
Michal <michalruth144@gmail.com>
2022-12-04 23:34:32 +0200

    fix switch case

--- main.c
+++ main.c
@@ -5,38 +5,59 @@ int main ()
 {
     
     int mat[ROWS][COLUMNS];
-    char user_input='0';
+    char user_input;
     int i,j;
 
     scanf("%c", &user_input);
     while(user_input != 'D'){
-        switch ((int) user_input)
-        {
-            case (int) 'A':
-                set_mat(mat);
-                get_shortest_path_matrix(mat);
-                break;
+        if(user_input=='A'){
+            set_mat(mat);
+            get_shortest_path_matrix(mat);
+        }
+        else if(user_input== 'B'){
+            scanf("%d", &i);
+            scanf("%d", &j);
+            is_there_path(mat,i,j);
+
+        }
+        else if(user_input=='C'){
+            scanf("%d", &i);
+            scanf("%d", &j);
+            shortest_path(mat, i, j);
+
+
+        }
+
+        scanf("%c", &user_input);
+    }
+    // while(user_input != 'D'){
+    //     switch ((int) user_input)
+    //     {
+    //         case (int) 'A':
+    //             set_mat(mat);
+    //             get_shortest_path_matrix(mat);
+    //             break;
 
            
-               // 
+    //            // 
                 
-            case (int) 'B':
+    //         case (int) 'B':
                 
-                scanf("%d", &i);
-                scanf("%d", &j);
-                is_there_path(mat,i,j);
-                break;
-            case (int) 'C':
+    //             scanf("%d", &i);
+    //             scanf("%d", &j);
+    //             is_there_path(mat,i,j);
+    //             break;
+    //         case (int) 'C':
                 
-                scanf("%d", &i);
-                scanf("%d", &j);
-                shortest_path(mat, i, j);
-                break;
+    //             scanf("%d", &i);
+    //             scanf("%d", &j);
+    //             shortest_path(mat, i, j);
+    //             break;
         
 
-             case (int) 'D':
-                break;
-        }
-    }
+    //          case (int) 'D':
+    //             break;
+    //     }
+    // }
   return 0;
 }
